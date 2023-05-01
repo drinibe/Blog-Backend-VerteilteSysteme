9306aadf84e1be45630abfdabb54f7694a523665
drinibe <valdrin.daki@gmx.ch>
2023-05-01 17:50:46 +0200

    Blog Code-Test hinzugefügt

--- src/test/java/ch/hftm/BlogResourceTest.java
+++ src/test/java/ch/hftm/BlogResourceTest.java
@@ -7,15 +7,15 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GreetingResourceTest {
+public class BlogResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testBlogEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/blog")
           .then()
              .statusCode(200)
-             .body(is("Hallo, dies ist der beginn meiner Ära!"));
+             .body(is("Willkommen auf meinem Blog!"));
     }
 
 }
